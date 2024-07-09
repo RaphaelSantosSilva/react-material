79e7909658a5cb435d0eaa0cb3b65c0cc065d4b5
Raphael Santos <raaphaaelsaantos@gmail.com>
2024-07-08 22:37:46 -0300

    projeto react with material

---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+import React from "react"
+import Header from "../partials/Header/Header"
+
+const  Home = () => {
+  return (
+    <>
+        <Header />
+        <h1>Welcome to my application react router with material ui!</h1>
+    </>
+  )
+}
+
+export default Home
